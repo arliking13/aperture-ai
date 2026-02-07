ab921addc45f20863d03fecc3cd008c237569823
arliking13 <156680160+arliking13@users.noreply.github.com>
2026-02-07 18:40:26 -0500

    page update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -65,7 +65,7 @@ export default function PosingCoach() {
         
         const landmarker = await PoseLandmarker.createFromOptions(vision, {
           baseOptions: {
-            modelAssetPath: "https://storage.googleapis.com/mediapipe-models/pose_landmarker/pose_landmarker_lite/float16/1/pose_landmarker_lite.task",
+            modelAssetPath: "https://storage.googleapis.com/mediapipe-models/pose_landmarker/pose_landmarker_full/float16/1/pose_landmarker_full.task",
             delegate: "GPU"
           },
           runningMode: "VIDEO",
